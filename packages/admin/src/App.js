e4c132ffae259d13708d476ba87c9aecb7e01c24
Alexis Janvier <contact@alexisjanvier.net>
2019-06-16 17:35:50 +0200

    Display Teams list

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import jsonServerProvider from './dataProvider';
 
 const dataProvider = jsonServerProvider('http://localhost:3001');
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="leagues" list={ListGuesser} />
+    <Resource name="teams" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
   </Admin>
 );
 
